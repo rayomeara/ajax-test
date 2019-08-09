eec88b7609c64b1eff888f12d5c491d3567712da
Ubuntu <ubuntu@ip-172-31-83-110.ec2.internal>
2019-08-09 12:17:23 +0000

    Implement pagination

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const baseURL = "https://swapi.co/api/";
-
-function getData(type, cb) {
+function getData(url, cb) {
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", baseURL + type + "/");
+    xhr.open("GET", url);
     xhr.send();
     
     xhr.onreadystatechange = function() {
@@ -22,11 +20,26 @@ function getTableHeaders(obj) {
     return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
+
+function writeToDocument(url) {
     var el = document.getElementById("data");
     el.innerHTML = "";
-    getData(type, function(data) {
+    getData(url, function(data) {
         var tableRows = [];  
+        var pagination;
+        if( data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous);
+        }
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
         data.forEach(function(item) {
@@ -38,7 +51,7 @@ function writeToDocument(type) {
             });
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`
         
     });
 }
